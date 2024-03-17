06a84fdf5ed19a7b8dd98e8d9a1cd30c3ee9d2a7
Matthew <matthewhjt72@gmail.com>
2024-03-17 22:09:31 +0700

    added throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { DivisiModule } from './divisi/divisi.module';
+import { Throttle, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, DivisiModule],
+  imports: [
+    DatabaseModule, 
+    DivisiModule,
+    ThrottlerModule.forRoot(
+      [
+        {
+          ttl: 1000,
+          limit: 3,
+        },
+      ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
